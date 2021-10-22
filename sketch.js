76b90a29c1e3c145093e99f2d7d91e14f793923a
isChase <90922508+isChase@users.noreply.github.com>
2021-10-22 11:00:04 -0600

    loaded images for room, created "state" (determined by key pressed)

--- sketch.js
+++ sketch.js
@@ -6,35 +6,54 @@
 //
 // Needed for game: Character & map art, Jason sprite, objects (keys, gas, battery), inventory
 
+let state = "rest";
+let img;
 
-let x = 200;
-let y = 200;
+function preload(){
+  img = loadImage("assets/pixil-frame-0 (3).png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  loadImage("assets/pixil-frame-0 (3).png");
+  image(img, 0, 0);
 }
 
 function draw() {
-  background(220);
-  fill(0);
-  rect(x, y, 10, 10);
-  if (x >= windowWidth){
-    x = 0; 
-  }
+  background(255);
 }
 
-
-function keyPressed() {
+function findState() {
   if (keyIsDown(UP_ARROW)) {
-    y -= 10;
+    state = "up";
   } 
   if (keyIsDown(DOWN_ARROW)) {
-    y += 10;
-  }
+    state = "down";
+  } 
   if (keyIsDown(LEFT_ARROW)) {
-    x -= 10;
+    state = "left";
   } 
   if (keyIsDown(RIGHT_ARROW)) {
-    x += 10;
-  }  
+    state = "right";
+  } 
 }
+
+function displayRoom1() {
+  if (state === "up"){
+    loadImage("assets/pixil-frame-0 (3).png");
+  }
+  else if (state === "left"){
+    loadImage("assets/pixil-frame-0 (2).png");
+  }
+  else if (state === "right"){
+    loadImage("assets/pixil-frame-0 (1).png");
+  }
+
+  // else if (state === "down"){
+  //   loadImage("assets/pixil-frame-0 (x)");
+  // }
+
+
+}
+
+
